f434f839ae70c113232764b41b94abbb583f5c1c
Murthy Yanamandra <murthy.viny@gmail.com>
2021-02-27 22:27:40 +0530

    new adjustments

--- iphone/views.py
+++ iphone/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 
+iphone_price = 60000
 
 def new_launches(request):
-    return render(request, 'iphone/new launches.html', context={'iphone': 60000})
+    return render(request, 'iphone/new launches.html', context={'iphone': iphone_price})
 
 
 def new_phones(request):
-    return render(request, 'iphone/new phones.html', context={'iphone': 50000})
+    return render(request, 'iphone/new phones.html', context={'iphone': iphone_price})
 
 
 def popular_products(request):
-    return render(request, 'iphone/popular products.html', context={'iphone': 40000})
+    return render(request, 'iphone/popular products.html', context={'iphone': iphone_price})
 
 
 def best_selling(request):
-    return render(request, 'iphone/best selling.html', context={'iphone': 30000})
+    return render(request, 'iphone/best selling.html', context={'iphone': iphone_price})
 
 
 
